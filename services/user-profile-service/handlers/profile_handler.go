60b2db1a6be15fbbcd3c2c3e42a1d23943cb5b04
defskela <sashokafanas@gmail.com>
2025-04-28 22:33:58 +0300

    Улучшены докерфайлы, заменена логика profile service

--- services/user-profile-service/handlers/profile_handler.go
+++ services/user-profile-service/handlers/profile_handler.go
@@ -4,6 +4,8 @@ import (
 	"encoding/json"
 	"net/http"
 	"strings"
+
+	"github.com/google/uuid"
 	"github.com/monst/story-craft/services/user-profile-service/models"
 
 	"github.com/gin-gonic/gin"
@@ -22,19 +24,53 @@ func NewProfileHandler(db *gorm.DB) *ProfileHandler {
 func (h ProfileHandler) CreateProfile(c *gin.Context) {
 	var input models.InputProfile
 
-	requestUser := c.Request.Header.Get("x-user-object")
-	if err := json.Unmarshal([]byte(requestUser), &input); err != nil {
+	userID := c.Request.Header.Get("x-user-object")
+
+	// Структура для парсинга
+	var data map[string]interface{}
+
+	// Парсинг JSON
+	err := json.Unmarshal([]byte(userID), &data)
+	if err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": "Ошибка в формате JSON: проверьте правильность данных"})
+	}
+
+	// Извлечение значения userId
+	userId, ok := data["userId"].(string)
+	if !ok {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Ошибка: не удалось извлечь userId из JSON"})
+		return
+	}
+
+	input.UserID = userId
+
+	body := c.Request.Body
+	defer body.Close()
+
+	if err := c.ShouldBindJSON(&input); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Ошибка в формате JSON: проверьте правильность данных"})
+		return
+	}
+
+	userUUID, err := uuid.Parse(userId)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Ошибка: неверный формат user_id: " + userID})
 		return
 	}
 
 	if input.Username == "" {
 		c.JSON(http.StatusBadRequest, gin.H{"error": "Ошибка: не передали username"})
+		return
+	}
+
+	if input.Email == "" {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Ошибка: не передали email"})
+		return
 	}
 
 	// Проверка на существование пользователя с таким username
 	var existingProfile models.Profile
-	if err := h.db.Where("username = ?", input.Username).First(&existingProfile).Error; err == nil {
+	if err := h.db.Where("user_id = ?", userUUID).First(&existingProfile).Error; err == nil {
 		c.JSON(http.StatusConflict, gin.H{"error": "Пользователь с таким username уже существует"})
 		return
 	} else if err != gorm.ErrRecordNotFound {
@@ -43,7 +79,7 @@ func (h ProfileHandler) CreateProfile(c *gin.Context) {
 	}
 
 	profile := models.Profile{
-		UserID:    input.UserID,
+		UserID:    userId,
 		Username:  input.Username,
 		Email:     input.Email,
 		AvatarURL: input.AvatarURL,
@@ -59,19 +95,15 @@ func (h ProfileHandler) CreateProfile(c *gin.Context) {
 		return
 	}
 
-	c.JSON(http.StatusCreated, input)
+	c.JSON(http.StatusCreated, profile)
 }
 
 // Получение профиля по user_id
 func (h ProfileHandler) GetProfile(c *gin.Context) {
-	var input models.InputProfile
-
-	if input.Username == "" {
-		c.JSON(http.StatusBadRequest, gin.H{"error": "Ошибка: не передали username"})
-	}
+	userID := c.Request.Header.Get("x-user-object")
 
 	var profile models.Profile
-	if err := h.db.Where("username = ?", input.Username).First(&profile).Error; err != nil {
+	if err := h.db.Where("user_id = ?", userID).First(&profile).Error; err != nil {
 		if err == gorm.ErrRecordNotFound {
 			c.JSON(http.StatusNotFound, gin.H{"error": "Профиль не найден"})
 		} else {
@@ -85,20 +117,19 @@ func (h ProfileHandler) GetProfile(c *gin.Context) {
 
 // Обновление профиля
 func (h ProfileHandler) UpdateProfile(c *gin.Context) {
-	var input models.InputProfile
+	var input models.UpdateProfile
 
-	requestUser := c.Request.Header.Get("x-user-object")
-	if err := json.Unmarshal([]byte(requestUser), &input); err != nil {
+	userID := c.Request.Header.Get("x-user-object")
+	body := c.Request.Body
+	defer body.Close()
+
+	if err := c.ShouldBindJSON(&input); err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": "Ошибка в формате JSON: проверьте правильность данных"})
 		return
 	}
 
-	if input.Username == "" {
-		c.JSON(http.StatusBadRequest, gin.H{"error": "Ошибка: не передали username"})
-	}
-
 	var profile models.Profile
-	if err := h.db.Where("username = ?", input.Username).First(&profile).Error; err != nil {
+	if err := h.db.Where("user_id = ?", userID).First(&profile).Error; err != nil {
 		if err == gorm.ErrRecordNotFound {
 			c.JSON(http.StatusNotFound, gin.H{"error": "Профиль не найден"})
 		} else {
@@ -117,7 +148,7 @@ func (h ProfileHandler) UpdateProfile(c *gin.Context) {
 	}
 
 	// Получаем обновленный профиль
-	if err := h.db.Where("username = ?", input.Username).First(&profile).Error; err != nil {
+	if err := h.db.Where("user_id = ?", userID).First(&profile).Error; err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{"error": "Профиль успешно обновлен, но возникла ошибка при получении обновленных данных"})
 		return
 	}
@@ -127,21 +158,11 @@ func (h ProfileHandler) UpdateProfile(c *gin.Context) {
 
 // Удаление профиля
 func (h ProfileHandler) DeleteProfile(c *gin.Context) {
-	var input models.InputProfile
-
-	requestUser := c.Request.Header.Get("x-user-object")
-	if err := json.Unmarshal([]byte(requestUser), &input); err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": "Ошибка в формате JSON: проверьте правильность данных"})
-		return
-	}
-
-	if input.Username == "" {
-		c.JSON(http.StatusBadRequest, gin.H{"error": "Ошибка: не передали username"})
-	}
+	userID := c.Request.Header.Get("x-user-object")
 
 	// Проверка существования профиля перед удалением
 	var profile models.Profile
-	if err := h.db.Where("username = ?", input.Username).First(&profile).Error; err != nil {
+	if err := h.db.Where("user_id = ?", userID).First(&profile).Error; err != nil {
 		if err == gorm.ErrRecordNotFound {
 			c.JSON(http.StatusNotFound, gin.H{"error": "Профиль для удаления не найден"})
 		} else {
--- start-dev.sh
+++ start-dev.sh
@@ -1,15 +1,9 @@
 #!/bin/bash
 
-if [ ! -f .env ]; then
-  echo "Creating .env file from .env.example"
-  cp .env.example .env
-fi
-
-set -a # Automatically export all variables
-source .env
-set +a
-echo "API_GATEWAY_PORT in script: ${API_GATEWAY_PORT}" # Добавлено для проверки
-echo "Running docker compose..."
+# if [ ! -f .env ]; then
+#   echo "Creating .env file from .env.example"
+#   cp .env.example .env
+# fi
 
 docker compose -f docker-compose.db.yml -f docker-compose.dev.yml up --build
 
